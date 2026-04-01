9e45fd76b0585c60a2563e1fb72a2beb30a3de0e
Raj <INTERN_SUMIT@DS.INDIANOIL.IN>
2026-04-01 16:28:37 +0530

    disable db completely

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,36 @@
-const conn = require('mysql2');
+// const conn = require('mysql2');
 
-const conection = conn.createConnection({
-    host: 'dpg-d76fa2p4tr6s738qgp00-a',
-    user:'root',
-    password:'MYWqsCqN2qbxIfUhaIp5L2DSXUKwkoGL',
-    database:'aistudydb',
+// const conection = conn.createConnection({
+//     host: 'dpg-d76fa2p4tr6s738qgp00-a',
+//     user:'root',
+//     password:'MYWqsCqN2qbxIfUhaIp5L2DSXUKwkoGL',
+//     database:'aistudydb',
 
-})
-console.log('Database connected successfully');
+// })
+// console.log('Database connected successfully');
 
-module.exports = conection;
+// module.exports = conection;
+
+
+const mysql = require('mysql2');
+
+let connection;
+
+function connectDB() {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    connection.connect((err) => {
+        if (err) {
+            console.log("DB Error:", err);
+        } else {
+            console.log("Database connected successfully");
+        }
+    });
+}
+
+module.exports = { connectDB };
