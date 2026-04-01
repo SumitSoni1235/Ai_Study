4b0f77967acf7976d8824e59d9b9151e5ec05ccc
Raj <INTERN_SUMIT@DS.INDIANOIL.IN>
2026-04-01 16:59:49 +0530

    fix route path and setup

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 process.env.GLOBAL_AGENT_HTTP_PROXY = 'http://mkthoproxy.ds.indianoil.in:8080';
 const { bootstrap } = require('global-agent');
+const routes = require('./src/routes/route');
 bootstrap();
 
 const app= require('./app');
 
+
 const {invokeai} = require('./src/services/GenAi');
 invokeai();
-
+app.use('/',routes);
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
